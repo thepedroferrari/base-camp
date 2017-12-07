c6767ed9b6c5882e97cf98b54018bb4a71ac7bc4
Toni Suomalainen <toni.suomalainen@taitounited.fi>
2017-12-07 12:18:56 +0200

    Initial Woocommerce integration

--- functions.php
+++ functions.php
@@ -2,3 +2,22 @@
 
  // Bootstrapping theme
 require_once __DIR__ . '/app/bootstrap.php';
+
+function timber_set_product($post)
+{
+    global $product;
+
+    if (is_woocommerce()) {
+        $product = wc_get_product($post->ID);
+    }
+}
+
+add_action('after_setup_theme', 'woocommerce_support');
+function woocommerce_support()
+{
+    add_theme_support('woocommerce');
+}
+
+add_theme_support('wc-product-gallery-zoom');
+add_theme_support('wc-product-gallery-lightbox');
+add_theme_support('wc-product-gallery-slider');
